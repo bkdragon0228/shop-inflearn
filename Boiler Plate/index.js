09d2a8c73da3d417892054774da773aa1e797be4
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-15 01:33:24 +0900

    추가 : mongoose 설치 후 mongoDB를 연결하는 코드 추가

--- Boiler Plate/index.js
+++ Boiler Plate/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose
+    .connect(
+        'mongodb+srv://bkdragon:kbk003178*@shopping.5cw7f.mongodb.net/?retryWrites=true&w=majority',
+        { useNewUrlParser: true } // deprecation  경고를 없애기 위해
+    )
+    .then(() => console.lot('DB connected'))
+    .catch((err) => console.err(err));
 
 app.get('/', (req, res) => {
     res.send('hello world');
