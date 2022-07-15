2119b4b893c727f2d079283645e2631af1f186bf
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-15 13:31:43 +0900

    수정 : 스키마, 모델에 설명 추가

--- Boiler Plate/models/user.js
+++ Boiler Plate/models/user.js
@@ -1,35 +1,39 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = mongoose.Schema({
-//     name: {
-//         type: String,
-//         maxlength: 50,
-//     },
-//     email: {
-//         type: String,
-//         trim: true,
-//         unique: 1,
-//     },
-//     password: {
-//         type: String,
-//         minlength: 5,
-//     },
-//     lastname: {
-//         type: String,
-//         maxlength: 50,
-//     },
-//     role: {
-//         type: Number,
-//         default: 0,
-//     },
-//     token: {
-//         type: String,
-//     },
-//     tokenExp: {
-//         type: Number,
-//     },
-// });
+// 스키마
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1,
+    },
+    password: {
+        type: String,
+        minlength: 5,
+    },
+    lastname: {
+        type: String,
+        maxlength: 50,
+    },
+    // 관리잦와 일번 유저를 구분하기 위해!
+    role: {
+        type: Number,
+        default: 0,
+    },
+    image: String,
+    token: {
+        type: String,
+    },
+    // 토큰 유효기간
+    tokenExp: {
+        type: Number,
+    },
+});
 
-// const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-// module.exports = { User };
+module.exports = { User };
