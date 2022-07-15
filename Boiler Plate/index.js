6bade279a433550eccece2b09080b036013469fa
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-15 13:24:28 +0900

    수정 : 몽구스 버전 업그레이드, idex.js 코드 수정

--- Boiler Plate/index.js
+++ Boiler Plate/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
+const port = 5000;
 const mongoose = require('mongoose');
 
 const uri =
-    'mongodb+srv://bkdragon:kbk003178*@shopping.5cw7f.mongodb.net/shopping';
+    'mongodb+srv://bkdragon:kbk003178@shopping.5cw7f.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(uri, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+    })
     .then(() => console.log('DB connection'))
     .catch((err) => console.error(err));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world hi');
 });
 
-app.listen(5000);
+app.listen(port, () => console.log('listen'));
