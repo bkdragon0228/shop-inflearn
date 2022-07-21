5bb325707f57e19d6449c743f0fb2c255691b162
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-21 11:50:52 +0900

    추가 :  회원가입 기능 추가

--- Boiler Plate/client/src/_action/types.js
+++ Boiler Plate/client/src/_action/types.js
@@ -1,3 +1,5 @@
 // 타입만 따로 저장
 
 export const LOGIN_USER = 'login_user';
+
+export const REGISTER_USER = 'register_user';
--- Boiler Plate/client/src/components/views/RegisterPage/RegisterPage.js
+++ Boiler Plate/client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { loginUser } from '../../../_action/user_action';
+import { registerUser } from '../../../_action/user_action';
 import { useDispatch } from 'react-redux';
 
 import styled from 'styled-components';
@@ -15,27 +15,38 @@ const Form = styled.form`
     display: flex;
     flex-direction: column;
 `;
-const RegisterPage = () => {
+const RegisterPage = (props) => {
     const dispatch = useDispatch();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const navi = useNavigate();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
 
+        if (password !== confirmPassword) {
+            return alert('비밀번화와 비밀번호 확인이 다름니다.');
+        }
+
         let body = {
+            name,
             email: email,
             password: password,
         };
-        dispatch(loginUser(body)).then((res) => {
-            if (res.payload.loginSuccess) {
+        dispatch(registerUser(body)).then((res) => {
+            if (res.payload.success) {
                 navi('/');
+            } else {
+                alert('faild to sign up');
             }
         });
     };
-
+    const onNameHandler = (e) => {
+        setName(e.currentTarget.value);
+    };
     const onEmailHandler = (e) => {
         setEmail(e.currentTarget.value);
     };
@@ -43,19 +54,35 @@ const RegisterPage = () => {
     const onPasswordHandler = (e) => {
         setPassword(e.currentTarget.value);
     };
+
+    const onConfirmPasswordHandler = (e) => {
+        setConfirmPassword(e.currentTarget.value);
+    };
     return (
         <MainDiv>
             <Form onSubmit={onSubmitHandler}>
+                <label>Name</label>
+                <input type="text" value={name} onChange={onNameHandler} />
+
                 <label>Email</label>
                 <input type="email" value={email} onChange={onEmailHandler} />
-                <label>password</label>
+
+                <label>Password</label>
                 <input
                     type="password"
                     value={password}
                     onChange={onPasswordHandler}
                 />
+
+                <label>Confirm Password</label>
+                <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={onConfirmPasswordHandler}
+                />
+
                 <br />
-                <button>Login</button>
+                <button>Sign Up</button>
             </Form>
         </MainDiv>
     );
