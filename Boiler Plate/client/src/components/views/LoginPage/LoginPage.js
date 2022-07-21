69b6180a1d2a297d3ef60be41c1eec4d0126fea0
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-21 10:57:36 +0900

    추가 : 로그인 페이지 기본 틀, 리덕스 적용 전까지

--- Boiler Plate/client/src/components/views/LoginPage/LoginPage.js
+++ Boiler Plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
+const MainDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let body = {
+            email: email,
+            password: password,
+        };
+
+        axios.post('/api/users/login', body).then((res) => {});
+    };
+
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value);
+    };
+
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value);
+    };
+    return (
+        <MainDiv>
+            <Form onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={onPasswordHandler}
+                />
+                <br />
+                <button>Login</button>
+            </Form>
+        </MainDiv>
+    );
 };
 
 export default LoginPage;
