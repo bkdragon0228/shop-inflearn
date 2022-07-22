1fa8eb50243c2b48d3410bd19123ae61adf9f794
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-23 00:17:39 +0900

    추가 : 업로드 dropdown을 제외한 기본 틀과 antd design 적용

--- shop/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ shop/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Typography, Button, Form, Input } from 'antd';
+
+const { Title } = Typography;
+const { TextArea } = Input;
 
 const UploadProductPage = () => {
-    return <div> UploadProductPage</div>;
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}>여행 상품 업로드</Title>
+            </div>
+
+            <Form>
+                <br />
+                <br />
+                <label>이름</label>
+                <Input value="안녕" />
+                <br />
+                <br />
+                <label>설명</label>
+                <TextArea />
+                <br />
+                <br />
+                <label>가격($)</label>
+                <Input />
+                <br />
+                <br />
+                <select>
+                    <option></option>
+                </select>
+                <br />
+                <br />
+                <Button>확인</Button>
+            </Form>
+        </div>
+    );
 };
 
 export default UploadProductPage;
