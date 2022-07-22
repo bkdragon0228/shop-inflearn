d284a5d16d2a26e2241dd9e6f70f600e6208952c
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-22 16:38:07 +0900

    추가 : 사진 업로드 페이지 생성, 라우트 추가, nav바 간단하게 수정

--- shop/client/src/App.js
+++ shop/client/src/App.js
@@ -5,11 +5,13 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import Auth from './hoc/auth';
+import UploadProductPage from './components/views/UploadProductPage/UploadProductPage';
 
 function App() {
     const AuthLandingPage = Auth(LandingPage, null);
     const AuthLoginPage = Auth(LoginPage, false);
     const AuthRegisterPage = Auth(RegisterPage, false);
+    const AuthUploadProductPage = Auth(UploadProductPage, true); // 로그인한 사람만
     return (
         <Router>
             <NavBar />
@@ -17,6 +19,10 @@ function App() {
                 <Route path="/" element={<AuthLandingPage />} />
                 <Route path="/login" element={<AuthLoginPage />} />
                 <Route path="/register" element={<AuthRegisterPage />} />
+                <Route
+                    path="/product/upload"
+                    element={<AuthUploadProductPage />}
+                />
             </Routes>
         </Router>
     );
--- shop/client/src/components/views/NavBar/NavBar.js
+++ shop/client/src/components/views/NavBar/NavBar.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <Link to="/register">Signup</Link>
+            <Link to="/login">Signin</Link>
+        </div>
+    );
 };
 
 export default NavBar;
