40a7b57c8274b73a7be689554b4431d3d2bc6e4a
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-23 00:46:41 +0900

    추가 : state와 이벤트리스너 추가

--- shop/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ shop/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
 const UploadProductPage = () => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState('');
+    const [continent, setContinent] = useState(1);
+
+    const [images, setImages] = useState([]);
+
+    const nameChangeHandler = (e) => {
+        setName(e.currentTarget.value);
+    };
+    const descriptionChangeHandler = (e) => {
+        setDescription(e.currentTarget.value);
+    };
+    const priceChangeHandler = (e) => {
+        setPrice(e.currentTarget.value);
+    };
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}>여행 상품 업로드</Title>
             </div>
 
-            <Form>
+            <Form onSubmit>
                 <br />
                 <br />
                 <label>이름</label>
-                <Input value="안녕" />
+                <Input value={name} onChange={nameChangeHandler} />
                 <br />
                 <br />
                 <label>설명</label>
-                <TextArea />
+                <TextArea
+                    value={description}
+                    onChange={descriptionChangeHandler}
+                />
                 <br />
                 <br />
                 <label>가격($)</label>
-                <Input />
+                <Input value={price} onChange={priceChangeHandler} />
                 <br />
                 <br />
                 <select>
