c25376ae49d47a00a8d0bb1aaeb5db5c7ab0edd9
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-24 15:24:56 +0900

    추가 :ㅣ이미지 파일을 전송하기 위해 폼전송
    axios.post로 백엔드로 보낸다. 아직 해당 라우트가 서버에 없다.

--- shop/client/src/components/views/LoginPage/LoginPage.js
+++ shop/client/src/components/views/LoginPage/LoginPage.js
@@ -32,6 +32,7 @@ const LoginPage = () => {
 
         // 액션을 만들고 dispatch로 발생시킨다.
         dispatch(loginUser(body)).then((res) => {
+            console.log(res);
             if (res.payload.loginSuccess) {
                 navi('/');
             }
--- shop/client/src/utils/FileUpload.js
+++ shop/client/src/utils/FileUpload.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 const FileUpload = () => {
+    const dropHandler = (files) => {
+        // 이미지 전송에 필요한 form 전송
+        let formData = new FormData();
+        const config = {
+            Headers: { 'content-type': 'multipart/form-data' },
+        };
+        formData.append('file', files[0]);
+
+        axios.post('/api/product/image', formData, config).then((res) => {
+            if (res.data.success) {
+            } else {
+                alert('fail to file upload');
+            }
+        });
+    };
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+            <Dropzone onDrop={dropHandler}>
                 {({ getRootProps, getInputProps }) => (
                     <section>
                         <div
