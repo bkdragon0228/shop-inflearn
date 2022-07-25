0fb8d0cb1ad2134a3825f7a2861fb17c7508f351
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-25 13:52:36 +0900

    수정 : axios 통신하는 부분을 수정
    async, await 구문으로 바꿔봤다.

--- shop/client/src/components/views/LandingPage/LandingPage.js
+++ shop/client/src/components/views/LandingPage/LandingPage.js
@@ -2,15 +2,17 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const LandingPage = () => {
+    const landingProducts = async () => {
+        try {
+            const responce = await axios.post('/api/product/products');
+            const data = responce.data;
+            console.log(data);
+        } catch (err) {
+            alert('상품을 가져오는데 실패했습니다.');
+        }
+    };
     useEffect(() => {
-        // 상품 필터링을 위해 post로
-        axios.post('/api/product/products').then((res) => {
-            if (res.data.success) {
-                console.log(res.data.productsInfo);
-            } else {
-                alert('상품을 가져오는데 실패했습니다.');
-            }
-        });
+        landingProducts();
     }, []);
     return <div>LandingPage</div>;
 };
