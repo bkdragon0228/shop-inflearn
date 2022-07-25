8800224f2cef4ea1565edbb93f8356f706efdf8f
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-25 15:13:00 +0900

    추가 : 받아온 데이터를 넣을 card 추가
    antd 라이브러리를 사용해서 받아온 데이터를 보여줄 ui 생성

--- shop/client/src/components/views/LandingPage/LandingPage.js
+++ shop/client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,67 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Col, Row, Card } from 'antd';
+import {
+    RocketOutlined,
+    EditOutlined,
+    EllipsisOutlined,
+    SettingOutlined,
+} from '@ant-design/icons';
+
+const { Meta } = Card;
 
 const LandingPage = () => {
+    const [products, setProducts] = useState([]);
+
     const landingProducts = async () => {
-        try {
-            const responce = await axios.post('/api/product/products');
-            const data = responce.data;
-            console.log(data);
-        } catch (err) {
-            alert('상품을 가져오는데 실패했습니다.');
-        }
+        const responce = await axios.post('/api/product/products');
+        const data = responce.data;
+        setProducts(data.productsInfo);
     };
+
     useEffect(() => {
         landingProducts();
     }, []);
-    return <div>LandingPage</div>;
+
+    const renderCards = products.map((product, index) => {
+        return (
+            <Col lg={6} md={8} xs={24} key={index}>
+                <Card
+                    cover={
+                        <img
+                            style={{ width: '100%', maxHeight: '150px' }}
+                            src={`http://localhost:5000/${product.images[0]}`}
+                        />
+                    }
+                    actions={[
+                        <SettingOutlined key="setting" />,
+                        <EditOutlined key="edit" />,
+                        <EllipsisOutlined key="ellipsis" />,
+                    ]}
+                >
+                    <Meta
+                        title={product.title}
+                        description={`$${product.price}`}
+                    />
+                </Card>
+            </Col>
+        );
+    });
+    return (
+        <div style={{ width: '75%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h2>
+                    Let's travel anywhere <RocketOutlined />
+                </h2>
+            </div>
+
+            <Row gutter={[16, 16]}>{renderCards}</Row>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>더보기</button>
+            </div>
+        </div>
+    );
 };
 
 export default LandingPage;
