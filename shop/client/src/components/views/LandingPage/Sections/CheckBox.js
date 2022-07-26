badec9ef4f844c552d3b3d8d115bdf2e9504f69b
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-26 15:39:33 +0900

    추가 : checkbox의 onChange 함수 구현

--- shop/client/src/components/views/LandingPage/Sections/CheckBox.js
+++ shop/client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Collapse, Checkbox } from 'antd';
 const { Panel } = Collapse;
 
 const CheckBox = ({ list }) => {
+    const [checked, setChecked] = useState([]);
+
+    const handleToggle = (id) => {
+        // 누른 것의 index를 구하고
+        const currentIndex = checked.indexOf(id);
+
+        // 전체 checked 된 state에서 현재 누른 checkbox가 없다면
+        const newChecked = [...checked];
+        if (currentIndex === -1) {
+            // state에 넣어준다.
+            newChecked.push(id);
+        } else {
+            // 없다면 뺴주고
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
     const renderCheckboxList = () =>
         // 가져오는 속도 차이가 있을 수 있어 list가 있는 지 확인
         list &&
         list.map((value, index) => (
             <React.Fragment key={index}>
-                <Checkbox onChange>
-                    <span>{value.name}</span>
-                </Checkbox>
+                <Checkbox
+                    onChange={() => handleToggle(value._id)}
+                    checked={checked.indexOf(value._id) === -1 ? false : true}
+                />
+                <span>{value.name}</span>
             </React.Fragment>
         ));
     // 의미없는 div 사용을 피하려고 React.Fragment를 사용
