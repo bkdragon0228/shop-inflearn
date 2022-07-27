6056d6f99ef29bae1ccf802737d8237c4139114d
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-27 13:23:06 +0900

    추가 : radiobox ui 완성

--- shop/client/src/components/views/LandingPage/Sections/RadioBox.js
+++ shop/client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Collapse } from 'antd';
+import React, { useState } from 'react';
+import { Collapse, Radio } from 'antd';
 const { Panel } = Collapse;
 
 const RadioBox = ({ list, handleFilters }) => {
+    const [Value, setValue] = useState(0); //price의 _id값을 넣어줌
+    const renderRadioBox = () =>
+        list &&
+        list.map((value) => (
+            <React.Fragment key={value._id}>
+                <Radio value={value._id}>{value.name}</Radio>
+            </React.Fragment>
+        ));
+
+    const handleChange = (e) => {
+        //Value를 클릭한 radio의 value로 바꿔줌
+        setValue(e.target.value);
+        handleFilters(e.target.value);
+    };
     return (
         <div>
             <Collapse defaultActiveKey={['1']}>
                 <Panel header="Price" key="1">
-                    {}
+                    <Radio.Group value={Value} onChange={handleChange}>
+                        {renderRadioBox()}
+                    </Radio.Group>
                 </Panel>
             </Collapse>
         </div>
